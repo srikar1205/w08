ef277d5beeced25b613e28d7d62e441f4354de6e
srikar patle <srikar1205@gmail.com>
2018-10-18 18:22:46 -0500

    done

---
+++ ajax.js
@@ -0,0 +1,18 @@
+(function ($) {
+
+    $('#btnLoadText').click(function () { $("#showResult").load("show.txt"); });
+    $('#btnAjax').click(function () { callRestAPI() });
+  
+    // Perform an asynchronous HTTP (Ajax) API request.
+    function callRestAPI() {
+      var root = 'https://jsonplaceholder.typicode.com'
+      $.ajax({
+        url: root + '/posts/4',
+        method: 'GET'
+      }).then(function (response) {
+        console.log(response);
+        $('#showResult').html(response.body);
+      });
+    }
+  })(jQuery);
+  
